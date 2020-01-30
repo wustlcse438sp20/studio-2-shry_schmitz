4eee1db5c5f43d2cfc49f6c592f7a27324694eba
Ethan Shry <ethan.shry@wustl.edu>
2020-01-30 15:17:07 -0600

    done with stuido

--- app/src/main/java/com/example/studio2/ChangeActivity.kt
+++ app/src/main/java/com/example/studio2/ChangeActivity.kt
@@ -1,18 +1,71 @@
 package com.example.studio2
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_change.*
+import kotlinx.android.synthetic.main.fragment_deposit.*
+import kotlinx.android.synthetic.main.fragment_withdraw.*
 
 class ChangeActivity : AppCompatActivity() {
+
+    var isDeposit = true
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change)
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val fragment = DepositFragment()
+        //var bundle = Bundle()
+
+        val trans = supportFragmentManager.beginTransaction()
+        trans.replace(R.id.contaner, fragment)
+        trans.commit()
+    }
+
+    fun returnWithdraw(view: View) {
+        val result = Intent()
+        result.putExtra("isDeposit", false)
+        result.putExtra("value", withdraw_value.text.toString().toInt())
+        setResult(Activity.RESULT_OK, result)
+        finish();
+    }
+
+    fun returnDeposit(view: View) {
+        val result = Intent()
+        result.putExtra("isDeposit", true)
+        result.putExtra("value", deposit_value.text.toString().toInt())
+        setResult(Activity.RESULT_OK, result)
+        finish();
+    }
+
+    fun changeFragment(view: View) {
+        if (!isDeposit) {
+            val fragment = DepositFragment()
+            //var bundle = Bundle()
+
+            val trans = supportFragmentManager.beginTransaction()
+            trans.replace(R.id.contaner, fragment)
+            trans.commit()
+        } else {
+            val fragment = WithdrawFragment()
+            //var bundle = Bundle()
+
+            val trans = supportFragmentManager.beginTransaction()
+            trans.replace(R.id.contaner, fragment)
+            trans.commit()
+        }
+        isDeposit = !isDeposit
     }
 
 }
--- app/src/main/java/com/example/studio2/MainActivity.kt
+++ app/src/main/java/com/example/studio2/MainActivity.kt
@@ -1,19 +1,52 @@
 package com.example.studio2
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val GET_CHANGE = 1
+
+    private var balance = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        value_display.text = "$balance"
+
+    }
+
+    fun navToChangeActivity(view: View) {
+        val i: Intent = Intent(this, ChangeActivity::class.java)
+        startActivityForResult(i, GET_CHANGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GET_CHANGE) {
+            Log.d("GOT CHANGE", data.toString())
+            if (resultCode == Activity.RESULT_OK) {
+                val isDeposit = data?.extras?.getBoolean("isDeposit")
+                val amount = data?.extras?.getInt("value")
+                if (isDeposit != null && amount != null) {
+                    if (isDeposit) {
+                        balance += amount
+                    } else {
+                        balance -= amount
+                    }
+                    value_display.text = "$balance"
+                }
+
+            }
+        }
     }
 
 }
